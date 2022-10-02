0c6f42495f91d14ae5f6c8ce086b353ec149673a
MaxMalts <max-kurt@bk.ru>
2022-10-02 23:17:08 +0300

    Added styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import './App.css';
 import cardsData from './assets/data.json';
 import Card from './components/Card.js';
+import styles from './App.module.css';
+
 
 function App() {
-  return (
-    <div className="App">
-      {cardsData.map(item => <Card key={item.title} cardData={item}></Card>)}
-    </div>
-  );
+    return (
+        <div className={styles.app}>
+            <main className={styles.pageContainer}>
+                {cardsData.map(item => (
+                    <div key={item.title} className={styles.card}>
+                        <Card cardData={item}></Card>
+                    </div>
+                ))}
+            </main>
+        </div>
+    );
 }
 
 export default App;
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,7 @@
 import {useState} from 'react';
 import like_unliked from '../assets/like_unliked.svg';
 import like_liked from '../assets/like_liked.svg';
+import styles from './Card.module.css';
 
 export default function Card({cardData}) {
     const [curLikes, setCurLikes] = useState(cardData.currentLikes);
@@ -13,12 +14,15 @@ export default function Card({cardData}) {
     console.log(cardData);
 
     return (
-        <div>
-            <div>{cardData.title}</div>
-            <div>{cardData.text}</div>
-            <div>
-                {curLikes}
-                <img src={liked ? like_liked : like_unliked} alt='' onClick={onLikeClick}/>
+        <div className={styles.container}>
+            <h3 className={styles.title}>{cardData.title}</h3>
+            <p className={styles.description}>{cardData.text}</p>
+
+            <div className={styles.footer}>
+                <span className={styles.likesAmt}>{curLikes}</span>
+                <button className={styles.likeBtn} onClick={onLikeClick}>
+                    <img src={liked ? like_liked : like_unliked} alt='' />
+                </button>
             </div>
         </div>
     )
