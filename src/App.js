b6a2ef0e4bbdec265ba25ccbd7eb964240350c3d
MaxMalts <max-kurt@bk.ru>
2022-10-09 01:41:14 +0400

    Made cards loading async

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import cardsData from './assets/data/articles.json';
+import React from 'react';
+import {getArticles} from './assets/helpers/get-articles';
 import Card from './components/Card.js';
 import styles from './App.module.css';
 
 
-function App() {
-    return (
-        <div className={styles.app}>
-            <main className={styles.pageContainer}>
-                {cardsData.map(item => (
-                    <div key={item.articleId} className={styles.card}>
-                        <Card
-                            title={item.title}
-                            text={item.text}
-                            currentLikes={item.currentLikes}
-                            commentsCount={item.commentsCount}
-                        />
-                    </div>
-                ))}
-            </main>
-        </div>
-    );
+export class App extends React.Component {
+    state = {
+        loading: true,
+        cards: []
+    }
+
+    componentDidMount() {
+        getArticles().then(cards => {
+            this.setState({
+                loading: false,
+                cards: cards
+            });
+        })
+    }
+
+    render() {
+        return (
+            <div className={styles.app}>
+                <main className={styles.pageContainer}>
+                    {this.state.loading
+                        ? 'Loading...'
+                        : this.state.cards.map(item => (
+                            <div key={item.articleId} className={styles.card}>
+                                <Card
+                                    title={item.title}
+                                    text={item.text}
+                                    currentLikes={item.currentLikes}
+                                    commentsCount={item.commentsCount}
+                                />
+                            </div>
+                        ))
+                    }
+                </main>
+            </div>
+        );
+    }
 }
 
 export default App;
