56f2ee5f77f87f19f57523d30a574a4bdbf084a4
MaxMalts <max-kurt@bk.ru>
2022-10-23 03:47:53 +0400

    Implemented comments sorting

--- src/components/CommentsSection.js
+++ src/components/CommentsSection.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, {useId} from 'react';
 import Comment from './Comment.js';
 import submitIcon from '../assets/icons/send.svg'
 import styles from './CommentsSection.module.scss';
 import baseStyles from '../assets/styles/base.module.scss';
 import {getComments} from '../assets/helpers/get-comments-by-article.js';
+import uuid from 'react-uuid';
 
 export default class CommentsSection extends React.Component {
     state = {
         loading: true,
         comments: []
     }
+
+    id;
 
     constructor(props) {
         super(props);
@@ -19,7 +22,9 @@ export default class CommentsSection extends React.Component {
         this.commentForm = React.createRef();
         this.onCommentDelete = this.onCommentDelete.bind(this);
         this.onCommentSubmit = this.onCommentSubmit.bind(this);
-    }
+
+        this.id = uuid();
+        console.log(this.id);}
 
     componentDidMount() {
         getComments(this.props.articleId).then(comments => {
@@ -28,6 +33,36 @@ export default class CommentsSection extends React.Component {
                 comments: [...comments]
             });
         })
+    }
+
+    onSortChange(value) {
+        let comparator = null;
+        if (value === 'date') {
+            comparator = (item1, item2) => {
+                let date1 = Date.parse(item1.date);
+                let date2 = Date.parse(item2.date);
+                if (date1 < date2) {
+                    return 1;
+                } else if (date1 === date2) {
+                    return 0;
+                } else {
+                    return -1;
+                }
+            };
+
+        } else {
+            comparator = (item1, item2) => {
+                if (item1.currentLikes < item2.currentLikes) {
+                    return 1;
+                } else if (item1.currentLikes === item2.currentLikes) {
+                    return 0;
+                } else {
+                    return -1;
+                }
+            };
+        }
+
+        this.setState({...this.state, comments: [...this.state.comments].sort(comparator)});
     }
 
     onCommentDelete(id) {
@@ -71,6 +106,30 @@ export default class CommentsSection extends React.Component {
     render() {
         return (
             <div className={styles.commentsContainer}>
+                <div className={styles.header}>
+                    <div className={styles.sortsContainer}>
+                        <span>Sort by: </span>
+
+                        <input
+                            id={this.id + "-sort-by_date"} className={styles.sortsRadio}
+                            type="radio"  name={this.id + "-sort-by"} value="date"
+                            onChange={event => this.onSortChange(event.target.value)}
+                        />
+                        <label
+                            className={styles.sortsLabel} htmlFor={this.id + "-sort-by_date"}
+                        >date</label>
+
+                        <input
+                            id={this.id + "-sort-by_likes"} className={styles.sortsRadio}
+                            type="radio" name={this.id + "-sort-by"} value="likes"
+                            onChange={event => this.onSortChange(event.target.value)}
+                        />
+                        <label
+                            className={styles.sortsLabel} htmlFor={this.id + "-sort-by_likes"}
+                        >likes</label>
+                    </div>
+                </div>
+
                 {this.state.loading
                     ? 'Loading...'
                     : this.state.comments.map(item => (
@@ -88,14 +147,16 @@ export default class CommentsSection extends React.Component {
                     onSubmit={this.onCommentSubmit}
                 >
                     <input
-                        className={baseStyles.inputField + ' ' + styles.nameInput} ref={this.nameInput} type='text'
-                        name='name'
-                        placeholder='Enter your name' maxLength='50' required
+                        className={baseStyles.inputField + ' ' + styles.nameInput}
+                        ref={this.nameInput} type="text"
+                        name="name"
+                        placeholder="Enter your name" maxLength="50" required
                     />
                     <textarea
-                        className={baseStyles.inputField + ' ' + styles.commentInput} ref={this.commentInput}
-                        name='comment'
-                        placeholder='Enter your comment'
+                        className={baseStyles.inputField + ' ' + styles.commentInput}
+                        ref={this.commentInput}
+                        name="comment"
+                        placeholder="Enter your comment"
                         required
                     />
                     <button className={baseStyles.button + ' ' + styles.submitBtn} type='submit'>
