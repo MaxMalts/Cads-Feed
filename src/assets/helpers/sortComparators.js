538ea3be9bb8df99dd4a64142722010e26f0c439
MaxMalts <max-kurt@bk.ru>
2022-11-12 18:59:24 +0400

    Minor fixes

--- src/assets/helpers/sortComparators.js
+++ src/assets/helpers/sortComparators.js
@@ -1,21 +1,10 @@
 export const dateComparator = (date1, date2) => {
-    let date1n = Date.parse(date1);
-    let date2n = Date.parse(date2);
-    if (date1n < date2n) {
-        return 1;
-    } else if (date1n === date2n) {
-        return 0;
-    } else {
-        return -1;
-    }
+    const date1n = Date.parse(date1);
+    const date2n = Date.parse(date2);
+
+    return date2n - date1n;
 };
 
 export const numComparator = (val1, val2) => {
-    if (val1 < val2) {
-        return 1;
-    } else if (val1 === val2) {
-        return 0;
-    } else {
-        return -1;
-    }
+    return val2 - val1;
 }
