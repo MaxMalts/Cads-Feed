a5aa9fc88e0f88cb57adb18086d4d661bd1ebb6f
MaxMalts <max-kurt@bk.ru>
2022-11-13 21:54:35 +0400

    Added main pages routes to stup pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
+import {Routes, Route} from 'react-router-dom'
 import rootReducer from './store/reducers/rootReducer';
+import HomePage from './pages/HomePage.js';
 import CardsFeed from './components/CardsFeed.js';
+import CardPage from './pages/CardPage.js';
 import styles from './App.module.scss';
 
 const store = createStore(rootReducer);
@@ -10,7 +13,11 @@ const store = createStore(rootReducer);
 const App = () => (
     <Provider store={store}>
         <div className={styles.app}>
-            <CardsFeed/>
+            <Routes>
+                <Route path='/' element={<HomePage/>}/>
+                <Route path='/articles' element={<CardsFeed/>}/>
+                <Route path='/articles/:articleId' element={<CardPage/>}/>
+            </Routes>
         </div>
     </Provider>
 );
