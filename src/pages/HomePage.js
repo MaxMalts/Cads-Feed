d1bb496a149a95392f92c7fbbc23c41ac99701fe
MaxMalts <max-kurt@bk.ru>
2022-11-14 00:08:51 +0400

    Marked up home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,13 @@
+import {Link} from 'react-router-dom';
+import styles from './HomePage.module.scss';
+
 export default function HomePage() {
     return (
-        <div>Home Page</div>
+        <div className={styles.container}>
+            <span className={styles.text}>
+                Welcome to my portfolio React application!
+            </span>
+            <Link className={styles.cardsLink} to='/articles'>Go to cards feed ></Link>
+        </div>
     )
 }
