35aafe00a02a36dee94798261305addb51e7e592
MaxMalts <max-kurt@bk.ru>
2022-11-21 22:50:46 +0300

    Added useReducer to new card fields

--- src/components/NewCard.js
+++ src/components/NewCard.js
@@ -1,14 +1,39 @@
-import React, {useRef} from 'react';
+import React, {useReducer} from 'react';
 import styles from './NewCard.module.scss';
 import baseStyles from '../assets/styles/base.module.scss';
 
+const actionSetTitleField = 'SET_TITLE_FIELD';
+const actionSetDescrField = 'SET_DESCRIPTION_FIELD';
+
+function fieldsReducer(state, action) {
+    switch(action.type) {
+        case actionSetTitleField: {
+            return {
+                ...state,
+                title: action.payload.title
+            };
+        }
+        case actionSetDescrField: {
+            return {
+                ...state,
+                description: action.payload.description
+            };
+        }
+        default: {
+            return state;
+        }
+    }
+}
+
 export default function NewCard({onCreated}) {
-    const titleInput = useRef(null);
-    const descriptionInput = useRef(null);
+    const [fields, fieldsDispatch] = useReducer(
+        fieldsReducer,
+        {title: '', description: ''}
+    );
 
     const onSubmit = event => {
         event.preventDefault();
-        onCreated(titleInput.current.value, descriptionInput.current.value);
+        onCreated(fields.title, fields.description);
     }
 
     return (
@@ -17,14 +42,30 @@ export default function NewCard({onCreated}) {
 
             <form className={styles.form} name='new-card' onSubmit={onSubmit}>
                 <input
-                    className={baseStyles.inputField + ' ' + styles.title} ref={titleInput} type='text'
+                    className={baseStyles.inputField + ' ' + styles.title}
+                    type='text'
                     name='card-title'
-                    placeholder='Card title' maxLength='50' required
+                    placeholder='Card title'
+                    maxLength='50'
+                    required
+                    onChange={event => fieldsDispatch({
+                        type: actionSetTitleField,
+                        payload: {
+                            title: event.target.value
+                        }
+                    })}
                 />
                 <textarea
-                    className={baseStyles.inputField + ' ' + styles.description} ref={descriptionInput}
+                    className={baseStyles.inputField + ' ' + styles.description}
                     name='card-description'
-                    placeholder='Card description' required
+                    placeholder='Card description'
+                    required
+                    onChange={event => fieldsDispatch({
+                        type: actionSetDescrField,
+                        payload: {
+                            description: event.target.value
+                        }
+                    })}
                 />
                 <button className={baseStyles.button + ' ' + styles.createBtn} type='submit'>Create</button>
             </form>
