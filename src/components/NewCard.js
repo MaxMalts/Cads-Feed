6dd5209e9f81567c32650f46ce77a2a0388e6d6f
MaxMalts <max-kurt@bk.ru>
2022-11-21 21:18:47 +0300

    Changed NewCard from class to function component

--- src/components/NewCard.js
+++ src/components/NewCard.js
@@ -1,40 +1,33 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import styles from './NewCard.module.scss';
 import baseStyles from '../assets/styles/base.module.scss';
 
-export default class NewCard extends React.Component {
-    constructor(props) {
-        super(props);
+export default function NewCard({onCreated}) {
+    const titleInput = useRef(null);
+    const descriptionInput = useRef(null);
 
-        this.titleInput = React.createRef();
-        this.descriptionInput = React.createRef();
-        this.onSubmit = this.onSubmit.bind(this);
-    }
-
-    onSubmit(event) {
+    const onSubmit = event => {
         event.preventDefault();
-        this.props.onCreated(this.titleInput.current.value, this.descriptionInput.current.value);
+        onCreated(titleInput.current.value, descriptionInput.current.value);
     }
 
-    render() {
-        return (
-            <div className={styles.newCardContainer}>
-                <h2 className={styles.heading}>Create new card</h2>
+    return (
+        <div className={styles.newCardContainer}>
+            <h2 className={styles.heading}>Create new card</h2>
 
-                <form className={styles.form} name='new-card' onSubmit={this.onSubmit}>
-                    <input
-                        className={baseStyles.inputField + ' ' + styles.title} ref={this.titleInput} type='text'
-                        name='card-title'
-                        placeholder='Card title' maxLength='50' required
-                    />
-                    <textarea
-                        className={baseStyles.inputField + ' ' + styles.description} ref={this.descriptionInput}
-                        name='card-description'
-                        placeholder='Card description' required
-                    />
-                    <button className={baseStyles.button + ' ' + styles.createBtn} type='submit'>Create</button>
-                </form>
-            </div>
-        );
-    }
+            <form className={styles.form} name='new-card' onSubmit={onSubmit}>
+                <input
+                    className={baseStyles.inputField + ' ' + styles.title} ref={titleInput} type='text'
+                    name='card-title'
+                    placeholder='Card title' maxLength='50' required
+                />
+                <textarea
+                    className={baseStyles.inputField + ' ' + styles.description} ref={descriptionInput}
+                    name='card-description'
+                    placeholder='Card description' required
+                />
+                <button className={baseStyles.button + ' ' + styles.createBtn} type='submit'>Create</button>
+            </form>
+        </div>
+    );
 }
